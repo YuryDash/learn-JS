5880697db87452b83d4e007b24869a5da990cb1d
Yury <ydaszukiewicz@gmail.com>
2022-10-02 19:11:25 +0300

    end

--- learn.js
+++ learn.js
@@ -337,79 +337,139 @@
 
 //hello again
 
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+//========== GAME guessing number==============
+// let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-let guesses = document.querySelector('.guesses');
-let lastResult = document.querySelector('.lastResult');
-let lowOrHi = document.querySelector('.lowOrHi');
+// let guesses = document.querySelector('.guesses');
+// let lastResult = document.querySelector('.lastResult');
+// let lowOrHi = document.querySelector('.lowOrHi');
 
-let guessSubmit = document.querySelector('.guessSubmit');
-let guessField = document.querySelector('.guessField');
+// let guessSubmit = document.querySelector('.guessSubmit');
+// let guessField = document.querySelector('.guessField');
 
-let guessCount = 1;
-let resetButton;
+// let guessCount = 1;
+// let resetButton;
 
-function checkGuess() {
-    let userGuess = Number(guessField.value);
-    if (guessCount === 1) {
-      guesses.textContent = 'Previous guesses: ';
-    }
-    guesses.textContent += userGuess + ' ';
+// function checkGuess() {
+//     let userGuess = Number(guessField.value);
+//     if (guessCount === 1) {
+//       guesses.textContent = 'Previous guesses: ';
+//     }
+//     guesses.textContent += userGuess + ' ';
   
-    if (userGuess === randomNumber) {
-      lastResult.textContent = 'Congratulations! You got it right!';
-      lastResult.style.backgroundColor = 'green';
-      lowOrHi.textContent = '';
-      setGameOver();
-    } else if (guessCount === 10) {
-      lastResult.textContent = '!!!GAME OVER!!!';
-      setGameOver();
-    } else {
-      lastResult.textContent = 'Wrong!';
-      lastResult.style.backgroundColor = 'red';
-      if(userGuess < randomNumber) {
-        lowOrHi.textContent = 'Last guess was too low!';
-      } else if(userGuess > randomNumber) {
-        lowOrHi.textContent = 'Last guess was too high!';
-      }
-    }
+//     if (userGuess === randomNumber) {
+//       lastResult.textContent = 'Congratulations! You got it right!';
+//       lastResult.style.backgroundColor = 'green';
+//       lowOrHi.textContent = '';
+//       setGameOver();
+//     } else if (guessCount === 10) {
+//       lastResult.textContent = '!!!GAME OVER!!!';
+//       setGameOver();
+//     } else {
+//       lastResult.textContent = 'Wrong!';
+//       lastResult.style.backgroundColor = 'red';
+//       if(userGuess < randomNumber) {
+//         lowOrHi.textContent = 'Last guess was too low!';
+//       } else if(userGuess > randomNumber) {
+//         lowOrHi.textContent = 'Last guess was too high!';
+//       }
+//     }
   
-    guessCount++;
-    guessField.value = '';
-    guessField.focus();
-  }
+//     guessCount++;
+//     guessField.value = '';
+//     guessField.focus();
+//   }
 
-  guessSubmit.addEventListener('click', checkGuess);
+//   guessSubmit.addEventListener('click', checkGuess);
 
-  function setGameOver() {
-    guessField.disabled = true;
-    guessSubmit.disabled = true;
-    resetButton = document.createElement('button');
-    resetButton.textContent = 'Start new game';
-    document.body.appendChild(resetButton);
-    resetButton.addEventListener('click', resetGame);
-  }
+//   function setGameOver() {
+//     guessField.disabled = true;
+//     guessSubmit.disabled = true;
+//     resetButton = document.createElement('button');
+//     resetButton.textContent = 'Start new game';
+//     document.body.appendChild(resetButton);
+//     resetButton.addEventListener('click', resetGame);
+//   }
 
 
-  function resetGame() {
-    guessCount = 1;
+//   function resetGame() {
+//     guessCount = 1;
   
-    var resetParas = document.querySelectorAll('.resultParas p');
-    for (let i = 0 ; i < resetParas.length ; i++) {
-      resetParas[i].textContent = '';
-    }
+//     var resetParas = document.querySelectorAll('.resultParas p');
+//     for (let i = 0 ; i < resetParas.length ; i++) {
+//       resetParas[i].textContent = '';
+//     }
   
-    resetButton.parentNode.removeChild(resetButton);
+//     resetButton.parentNode.removeChild(resetButton);
   
-    guessField.disabled = false;
-    guessSubmit.disabled = false;
-    guessField.value = '';
-    guessField.focus();
+//     guessField.disabled = false;
+//     guessSubmit.disabled = false;
+//     guessField.value = '';
+//     guessField.focus();
   
-    lastResult.style.backgroundColor = 'white';
+//     lastResult.style.backgroundColor = 'white';
   
-    randomNumber = Math.floor(Math.random() * 100) + 1;
-  }
+//     randomNumber = Math.floor(Math.random() * 100) + 1;
+//   }
+
+//========== GAME guessing number==============
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
